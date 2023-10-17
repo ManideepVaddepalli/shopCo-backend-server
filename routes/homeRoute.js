eec17ce11b3e6a5e688826d90c7e8449566b5c72
ManideepVaddepalli <manideep15168@gmail.com>
2023-10-17 18:36:32 +0530

    fourth

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.get("", (req, res) => {
   res.status(200).json({ mssg: "Working" });
 });
 
